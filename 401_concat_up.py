bf91dbe4a80595feda6cd925c37848016a102fe0
Qiao Wang <adelawqq@gmail.com>
2022-10-01 00:37:36 +0800

    change merge to join on index

--- 401_concat_up.py
+++ 401_concat_up.py
@@ -13,35 +13,41 @@ nrows = None
 # sample_frac = 0.1  # have to sample on training set to avoid out of memory
 
 def make_data(data_folder='data', make_set='train'):
-    base = pd.read_pickle('{}/base.pickle'.format(data_folder))
+    index_cols = ['user_id', 'product_id']
+    base = pd.read_pickle('{}/base.pickle'.format(data_folder)).set_index(index_cols)
 
     # read pre-created features
-    up_agg = pd.read_pickle('{}/up_agg.pickle'.format(data_folder))
-    up_purchase_r5 = pd.read_pickle('{}/up_purchase_r5.pickle'.format(data_folder))
-    up_days_since_last_purchase = pd.read_pickle('{}/up_days_since_last_purchase.pickle'.format(data_folder))
-    up_days_since_last_purchase_r5 = pd.read_pickle('{}/up_days_since_last_purchase_r5.pickle'.format(data_folder))
-    up_purchase_proba = pd.read_pickle('{}/up_purchase_proba.pickle'.format(data_folder))
-    up_purchase_proba_r5 = pd.read_pickle('{}/up_purchase_proba_r5.pickle'.format(data_folder))
-    up_days_not_purchase = pd.read_pickle('{}/up_days_not_purchase.pickle'.format(data_folder))
-    up_aisles_purchase_trend = pd.read_pickle('{}/up_aisles_purchase_trend.pickle'.format(data_folder))
-    up_departments_purchase_trend = pd.read_pickle('{}/up_departments_purchase_trend.pickle'.format(data_folder))
-    up_substitute_purchase = pd.read_pickle('{}/up_substitute_purchase.pickle'.format(data_folder))
-    up_organic_substitute_purchase = pd.read_pickle('{}/up_organic_substitute_purchase.pickle'.format(data_folder))
-    up_order_time = pd.read_pickle('{}/up_order_time.pickle'.format(data_folder))
+    up_agg = pd.read_pickle('{}/up_agg.pickle'.format(data_folder)).set_index(index_cols)
+    up_purchase_r5 = pd.read_pickle('{}/up_purchase_r5.pickle'.format(data_folder)).set_index(index_cols)
+    up_days_since_last_purchase = pd.read_pickle('{}/up_days_since_last_purchase.pickle'.format(data_folder)).set_index(index_cols)
+    up_days_since_last_purchase_r5 = pd.read_pickle('{}/up_days_since_last_purchase_r5.pickle'.format(data_folder)).set_index(index_cols)
+    up_purchase_proba = pd.read_pickle('{}/up_purchase_proba.pickle'.format(data_folder)).set_index(index_cols)
+    up_purchase_proba_r5 = pd.read_pickle('{}/up_purchase_proba_r5.pickle'.format(data_folder)).set_index(index_cols)
+    up_days_not_purchase = pd.read_pickle('{}/up_days_not_purchase.pickle'.format(data_folder)).set_index(index_cols)
+    up_aisles_purchase_trend = pd.read_pickle('{}/up_aisles_purchase_trend.pickle'.format(data_folder)).set_index(index_cols)
+    up_departments_purchase_trend = pd.read_pickle('{}/up_departments_purchase_trend.pickle'.format(data_folder)).set_index(index_cols)
+    up_substitute_purchase = pd.read_pickle('{}/up_substitute_purchase.pickle'.format(data_folder)).set_index(index_cols)
+    up_organic_substitute_purchase = pd.read_pickle('{}/up_organic_substitute_purchase.pickle'.format(data_folder)).set_index(index_cols)
+    up_order_time = pd.read_pickle('{}/up_order_time.pickle'.format(data_folder)).set_index(index_cols)
+    # up_word2vec_substitute_purchase = pd.read_pickle('{}/up_word2vec_substitute_purchase.pickle'.format(data_folder)).set_index(index_cols)
+    # up_word2vec_substitute_purchase_07 = pd.read_pickle('{}/up_word2vec_substitute_purchase_07.pickle'.format(data_folder)).set_index(index_cols)
 
-    users_features = pd.read_pickle('{}/users_features.pickle'.format(data_folder))
-    users_basket_size_trend = pd.read_pickle('{}/users_basket_size_trend.pickle'.format(data_folder))
-    users_orders_interval_trend = pd.read_pickle('{}/users_orders_interval_trend.pickle'.format(data_folder))
-    users_organic_purchases = pd.read_pickle('{}/users_organic_purchases.pickle'.format(data_folder))
-    users_organic_purchases_r5 = pd.read_pickle('{}/users_organic_purchases_r5.pickle'.format(data_folder))
-    users_order_time = pd.read_pickle('{}/users_order_time.pickle'.format(data_folder))
-    users_cluster = pd.read_pickle('{}/user_kmeans_cluster.pickle'.format(data_folder))
+    index_cols = ['user_id']
+    users_features = pd.read_pickle('{}/users_features.pickle'.format(data_folder)).set_index(index_cols)
+    users_basket_size_trend = pd.read_pickle('{}/users_basket_size_trend.pickle'.format(data_folder)).set_index(index_cols)
+    users_orders_interval_trend = pd.read_pickle('{}/users_orders_interval_trend.pickle'.format(data_folder)).set_index(index_cols)
+    users_organic_purchases = pd.read_pickle('{}/users_organic_purchases.pickle'.format(data_folder)).set_index(index_cols)
+    users_organic_purchases_r5 = pd.read_pickle('{}/users_organic_purchases_r5.pickle'.format(data_folder)).set_index(index_cols)
+    users_order_time = pd.read_pickle('{}/users_order_time.pickle'.format(data_folder)).set_index(index_cols)
+    # users_cluster = pd.read_pickle('{}/user_kmeans_cluster.pickle'.format(data_folder)).set_index(index_cols)
 
-    product_features_basic_agg = pd.read_pickle('{}/product_features_basic_agg.pickle'.format(data_folder))
-    product_organic_features = pd.read_pickle('{}/product_organic_features.pickle'.format(data_folder))
-    products_purchases_features = pd.read_pickle('{}/products_purchases_features.pickle'.format(data_folder))
-    product_purchase_cycle = pd.read_pickle('{}/product_purchase_cycle.pickle'.format(data_folder))
-    products_embedding = pd.read_pickle('{}/word2vec_prods_embedding_renamed.pickle'.format(data_folder))
+    index_cols = ['product_id']
+    product_features_basic_agg = pd.read_pickle('{}/product_features_basic_agg.pickle'.format(data_folder)).set_index(index_cols)
+    product_organic_features = pd.read_pickle('{}/product_organic_features.pickle'.format(data_folder)).set_index(index_cols)
+    products_purchases_features = pd.read_pickle('{}/products_purchases_features.pickle'.format(data_folder)).set_index(index_cols)
+    product_purchase_cycle = pd.read_pickle('{}/product_purchase_cycle.pickle'.format(data_folder)).set_index(index_cols)
+    products_embedding = pd.read_pickle('{}/product_embedding.pickle'.format(data_folder)).set_index(index_cols)
+    # product_sub_stats = pd.read_pickle('{}/product_sub_stats.pickle'.format(data_folder)).set_index(index_cols)
 
     if make_set == 'train':
         # get train users <user_id, order_id, product_id, reordered>
@@ -52,8 +58,8 @@ def make_data(data_folder='data', make_set='train'):
         base = base.loc[base['eval_set'] == 'test']
 
     # add reordered label to the train data
-    data_full_features = base.merge(up_agg, on=['user_id', 'product_id'], how='left').drop('eval_set', axis=1)
-    print(data_full_features[['user_id', 'product_id']].nunique())
+    data_full_features = base.join(up_agg, on=['user_id', 'product_id'], how='left').drop('eval_set', axis=1)
+    print(data_full_features.index.nunique())
 
     # select sub features to avoid out of memory
     # selected_users_features = ['user_id',
@@ -70,57 +76,60 @@ def make_data(data_folder='data', make_set='train'):
     #                              '_p_avg_first_reorder_diff', '_p_purchases_time_per_buyer',
     #                              '_p_sum_secondtime_purchase']
 
-    print('merge1')
+    print('join1')
     # attach all features to the unique user_id and product_id tuple
-    data_full_features = data_full_features.merge(up_purchase_r5, on=['user_id', 'product_id'], how='left')
+    data_full_features = data_full_features.join(up_purchase_r5, on=['user_id', 'product_id'], how='left')
     del up_purchase_r5
     # fill None with -1 as 0 has a meaning
-    print('merge2')
-    # data_full_features = data_full_features.merge(up_days_since_last_purchase, on=['user_id', 'product_id'],
+    print('join2')
+    # data_full_features = data_full_features.join(up_days_since_last_purchase, on=['user_id', 'product_id'],
     #                                               how='left').fillna(0)
-    data_full_features = data_full_features.merge(up_days_since_last_purchase, on=['user_id', 'product_id'],
+    data_full_features = data_full_features.join(up_days_since_last_purchase, on=['user_id', 'product_id'],
                                                   how='left')
     del up_days_since_last_purchase
-    print('merge3')
-    # data_full_features = data_full_features.merge(up_days_since_last_purchase_r5, on=['user_id', 'product_id'],
+    print('join3')
+    # data_full_features = data_full_features.join(up_days_since_last_purchase_r5, on=['user_id', 'product_id'],
     #                                               how='left').fillna(-1)
-    data_full_features = data_full_features.merge(up_days_since_last_purchase_r5, on=['user_id', 'product_id'],
+    data_full_features = data_full_features.join(up_days_since_last_purchase_r5, on=['user_id', 'product_id'],
                                                   how='left')
     del up_days_since_last_purchase_r5
-    print('merge4')
-    data_full_features = data_full_features.merge(up_purchase_proba, on=['user_id', 'product_id'], how='left')
-    data_full_features = data_full_features.merge(up_purchase_proba_r5, on=['user_id', 'product_id'], how='left')
-    data_full_features = data_full_features.merge(up_days_not_purchase, on=['user_id', 'product_id'], how='left')
-    data_full_features = data_full_features.merge(up_aisles_purchase_trend, on=['user_id', 'product_id'], how='left')
-    data_full_features = data_full_features.merge(up_departments_purchase_trend, on=['user_id', 'product_id'], how='left')
-    data_full_features = data_full_features.merge(up_substitute_purchase, on=['user_id', 'product_id'], how='left')
-    data_full_features = data_full_features.merge(up_organic_substitute_purchase, on=['user_id', 'product_id'], how='left')
-    data_full_features = data_full_features.merge(up_order_time, on=['user_id', 'product_id'], how='left')
+    print('join4')
+    data_full_features = data_full_features.join(up_purchase_proba, on=['user_id', 'product_id'], how='left')
+    data_full_features = data_full_features.join(up_purchase_proba_r5, on=['user_id', 'product_id'], how='left')
+    data_full_features = data_full_features.join(up_days_not_purchase, on=['user_id', 'product_id'], how='left')
+    data_full_features = data_full_features.join(up_aisles_purchase_trend, on=['user_id', 'product_id'], how='left')
+    data_full_features = data_full_features.join(up_departments_purchase_trend, on=['user_id', 'product_id'], how='left')
+    data_full_features = data_full_features.join(up_substitute_purchase, on=['user_id', 'product_id'], how='left')
+    data_full_features = data_full_features.join(up_organic_substitute_purchase, on=['user_id', 'product_id'], how='left')
+    data_full_features = data_full_features.join(up_order_time, on=['user_id', 'product_id'], how='left')
+    # data_full_features = data_full_features.join(up_word2vec_substitute_purchase, on=['user_id', 'product_id'], how='left')
+    # data_full_features = data_full_features.join(up_word2vec_substitute_purchase_07, on=['user_id', 'product_id'], how='left')
 
-    # data_full_features = data_full_features.merge(users_features[selected_users_features], on='user_id', how='inner')
-    data_full_features = data_full_features.merge(users_features, on='user_id', how='inner')
-    data_full_features = data_full_features.merge(users_basket_size_trend, on='user_id', how='inner')
-    data_full_features = data_full_features.merge(users_orders_interval_trend, on='user_id', how='inner')
-    data_full_features = data_full_features.merge(users_organic_purchases, on='user_id', how='left')
-    data_full_features = data_full_features.merge(users_organic_purchases_r5, on='user_id', how='left')
-    data_full_features = data_full_features.merge(users_order_time, on='user_id', how='left')
-    data_full_features = data_full_features.merge(users_cluster, on='user_id', how='left')
-
-    # data_full_features = data_full_features.merge(product_features[selected_product_features], on='product_id',
-    #                                               how='inner')
-    data_full_features = data_full_features.merge(product_features_basic_agg, on='product_id', how='inner')
+    # data_full_features = data_full_features.join(users_features[selected_users_features], on='user_id', how='inner')
+    data_full_features = data_full_features.join(users_features, on='user_id', how='inner')
+    data_full_features = data_full_features.join(users_basket_size_trend, on='user_id', how='inner')
+    data_full_features = data_full_features.join(users_orders_interval_trend, on='user_id', how='inner')
+    data_full_features = data_full_features.join(users_organic_purchases, on='user_id', how='left')
+    data_full_features = data_full_features.join(users_organic_purchases_r5, on='user_id', how='left')
+    data_full_features = data_full_features.join(users_order_time, on='user_id', how='left')
+    # data_full_features = data_full_features.join(users_cluster, on='user_id', how='left')
     # release memory
-    gc.collect(); del up_purchase_proba, up_purchase_proba_r5, up_days_not_purchase, users_features, product_features_basic_agg
+    gc.collect(); del up_purchase_proba, up_purchase_proba_r5, up_days_not_purchase, users_features, \
+        users_basket_size_trend, users_orders_interval_trend, users_organic_purchases, users_organic_purchases_r5, users_order_time
 
-    # data_full_features.to_pickle('data/merge_by_chunk_full.pickle')
-    print('merge5')
-    data_full_features = data_full_features.merge(products_purchases_features, on='product_id', how='left')
-    data_full_features = data_full_features.merge(product_purchase_cycle, on='product_id', how='left')
-    data_full_features = data_full_features.merge(product_organic_features, on='product_id', how='left')
+    data_full_features = improve_data_type(data_full_features)
+    print(data_full_features.head(1))
 
-    data_full_features = data_full_features.merge(products_embedding, on='product_id', how='left')
+    print('join5')
+    data_full_features = data_full_features.join(product_features_basic_agg, on='product_id', how='left')
+    data_full_features = data_full_features.join(products_purchases_features, on='product_id', how='left')
+    data_full_features = data_full_features.join(product_purchase_cycle, on='product_id', how='left')
+    data_full_features = data_full_features.join(product_organic_features, on='product_id', how='left')
 
-    print('merge5 done')
+    data_full_features = data_full_features.join(products_embedding, on='product_id', how='left')
+    # data_full_features = data_full_features.join(product_sub_stats, on='product_id', how='left')
+
+    print('join5 done')
     gc.collect(); del products_purchases_features
     # create extra features
     data_full_features['up_num_orders_since_last_purchase'] = data_full_features['user_total_orders'] - \
@@ -181,17 +190,12 @@ def make_data(data_folder='data', make_set='train'):
 
     # fill Null
     # data_full_features.fillna(0, inplace=True)
-    print(data_full_features.isnull().sum())
-
-    print(min(data_full_features['p_num_purchases']) >= 0)
     # clean memory
     gc.collect()
-    data_full_features = improve_data_type(data_full_features)
-
+    data_full_features = improve_data_type(data_full_features.reset_index())
     # change columns type to string
     data_full_features.columns = data_full_features.columns.astype('str')
-    print(data_full_features.columns)
-    print('{} set shape'.format(make_set))
+    print('finish creating {} set'.format(make_set))
     data_full_features.to_pickle('{}/{}_full_features.pickle'.format(data_folder, make_set))
 
 
