0283a92a302016b2d03aebb39fca217dbe59ce9a
Qiao Wang <adelawqq@gmail.com>
2022-10-13 11:39:47 +0800

    remove high corr cols, correct name, optimize diff

--- 306_up_order_interval.py
+++ 306_up_order_interval.py
@@ -13,20 +13,20 @@ up_purchase_time = pd.read_pickle(
 )
 
 up_orders = up_purchase_time[["user_id", "product_id", "order_number"]]
-up_orders["order_interval"] = up_orders.groupby(["user_id", "product_id"])[
-    "order_number"
-].diff()
+up_orders["order_interval"] = (
+    up_orders["order_number"]
+    - up_orders.groupby(["user_id", "product_id"])["order_number"].shift()
+)
 
 up_orders = up_orders.loc[~up_orders["order_interval"].isnull()]
 up_orders_interval = up_orders.groupby(["user_id", "product_id"]).agg(
-    {"order_interval": [list, "mean", "max", "min", "median", trend_d1, skewness]}
+    {"order_interval": [list, "max", "min", "mean", trend_d1, skewness]}
 )
 up_orders_interval.columns = [
     "up_order_intervals",
-    "up_mean_order_interval",
     "up_max_order_interval",
     "up_min_order_interval",
-    "up_median_order_interval",
+    "up_mean_order_interval",
     "up_order_interval_trend_d1",
     "up_order_interval_skewness",
 ]
@@ -47,17 +47,17 @@ orders_r5 = prior_order_details[
 up_purchase_time_r5 = up_purchase_time.loc[up_purchase_time.order_id.isin(orders_r5)]
 
 up_orders = up_purchase_time_r5[["user_id", "product_id", "order_number"]]
-up_orders["order_interval"] = up_orders.groupby(["user_id", "product_id"])[
-    "order_number"
-].diff()
+up_orders["order_interval"] = (
+    up_orders["order_number"]
+    - up_orders.groupby(["user_id", "product_id"])["order_number"].shift()
+)
 
 up_orders = up_orders.loc[~up_orders["order_interval"].isnull()]
 up_orders_interval = up_orders.groupby(["user_id", "product_id"]).agg(
-    {"order_interval": [list, "mean", "max", "min", "median", trend_d1, skewness]}
+    {"order_interval": [list, "max", "min", "median", trend_d1, skewness]}
 )
 up_orders_interval.columns = [
     "up_order_intervals_r5",
-    "up_mean_order_interval_r5",
     "up_max_order_interval_r5",
     "up_min_order_interval_r5",
     "up_median_order_interval_r5",
@@ -65,8 +65,8 @@ up_orders_interval.columns = [
     "up_order_interval_skewness_r5",
 ]
 
-up_orders_interval.drop("up_order_intervals", axis=1).to_pickle(
-    "data/up_orders_interval.pickle"
+up_orders_interval.drop("up_order_intervals_r5", axis=1).to_pickle(
+    "data/up_orders_interval_r5.pickle"
 )
 
 
